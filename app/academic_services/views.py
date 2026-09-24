2bc57699e7363f78edb35bcd974b00bc4546d4f5
ASUS <somchai081643@gmail.com>
2026-09-24 09:14:54 +0700

    Added customer.is_document_verified value in customer_account vew of academic_services views

--- app/academic_services/views.py
+++ app/academic_services/views.py
@@ -1312,6 +1312,8 @@ def customer_account():
         if not current_user.customer_info:
             account.customer_info = customer
             db.session.add(account)
+        if customer.is_document_verified is not None:
+            customer.is_document_verified = None
         db.session.add(customer)
         db.session.commit()
         flash('บันทึกข้อมูลเรียบร้อยแล้ว กรุณาเลือกแล็บ', 'success')
